f3f48c35660c5c28fe131cb3a4f259428e186d82
vsolovov <v.solovov@carx-tech.com>
2025-03-14 10:22:29 +0300

    Завершен урок 2.2 Основы С#

---
+++ 22CHBase.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+
+        /*
+        class House
+        {
+            public string street;
+            public int number;
+
+            public void Print()
+            {
+                Console.WriteLine($"{street}, {number}");
+            }
+        }
+        */
+        static void Main(string[] args)
+        {
+            /*
+            int a = 5;
+            int b = 5;
+            int c = 10;
+            if (b > a)
+            {
+                Console.WriteLine($"Число {b} больше числа {a}");
+            }
+            else if (b < a)
+            {
+                Console.WriteLine($"Число {b} меньше числа {a}");
+            }
+            else
+            {
+                Console.WriteLine($"Число {b} равно числу {a}");
+            }
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int i = 0;
+            while (i <= 10)
+            {
+                i++;
+                Console.WriteLine(i);
+            }
+
+            int[] numbers = new int[5];
+            int[] numbers2 = {5, 6, 7, 8, 9};
+            numbers2[4] = 15;
+            Console.WriteLine(numbers2[4]);
+
+            
+                void Helloworld()
+                {
+                    Console.WriteLine("Hello World");
+                }
+                void Sum(int a, int b)
+                {
+                    int sum = a + b;
+                    Console.WriteLine(sum);
+                }
+                
+                void Human(string name, int age)
+                {   
+                    Console.WriteLine($"{name}, {age}");
+                }
+                Human("Алексей", 25);
+
+            House tower = new House();
+            tower.street = "Красная";
+            tower.number = 50;
+
+            tower.Print();
+            */
+        }
+    }
+}
+Объяснить
